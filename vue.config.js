b7c52bacb87072e488f61793e78bff2f8b5321e3
Emmanuel ZIMMERT <ez@ladigitale.dev>
2022-02-06 18:46:37 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,23 @@
+const path = require('path')
+
 module.exports = {
 	publicPath: './',
 	outputDir: 'dist',
 	assetsDir: 'static',
-	productionSourceMap: false
+	productionSourceMap: false,
+	chainWebpack: config => {
+		config.plugin('copy').tap(args => {
+			args[0].push({
+				from: path.resolve(__dirname, 'README.md'),
+				to: path.resolve(__dirname, 'dist'),
+				toType: 'dir'
+			},
+			{
+				from: path.resolve(__dirname, 'LICENSE'),
+				to: path.resolve(__dirname, 'dist'),
+				toType: 'dir'
+			})
+			return args
+		})
+	}
 }
